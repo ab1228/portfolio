75157744454daedfa8f2a048906400ee5943daa6
ab1228 <alexb8734@gmail.com>
2020-01-16 23:47:07 -0600

    added contact page styled

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,6 +6,22 @@ module.exports = function (app) {
 
     // index route loads view.html
     app.get("/", function (req, res) {
+        res.render("index");
+    });
+
+    app.get("/about", function (req, res) {
+        res.render("about");
+    });
+
+    app.get("/contact", function (req, res) {
+        res.render("contact");
+    });
+
+    app.get("/portfolio", function (req, res) {
+        res.render("portfolio");
+    });
+
+    app.get('*', (req, res) => {
         res.render('index');
     });
 
