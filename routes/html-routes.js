e5757eb449a2d57f14c442ef7799e561aa4f3ce7
ab1228 <alexb8734@gmail.com>
2020-03-16 01:36:25 -0500

    updated routes and .gitignore

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -39,7 +39,35 @@ module.exports = function (app) {
         <h3>Message</h3>
         <p>${req.body.message}</p>`;
 
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: testAccount.user, // generated ethereal user
+                pass: testAccount.pass // generated ethereal password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
+
+        // send mail with defined transport object
+        let info = transporter.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: "bar@example.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: output // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
     })
 
